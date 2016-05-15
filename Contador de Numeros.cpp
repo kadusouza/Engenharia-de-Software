d676e6157ae2f36ee15c91931aff49243de73df2
Carlos Eduardo Assumpção Castro de Souza <kadu23977@gmail.com>
2016-05-15 15:53:53 -0300

    Correcao dos Numeros Primos
    Agora somente capta ate 8 numeros

--- Contador de Numeros.cpp
+++ Contador de Numeros.cpp
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
 int main() {
-	int n, i, j, k, vet[n], teste;
+	int n, i, j, vet[n];
 	int pares, impares, primos, soma;
+	int c = 2;
+	i = 0;
+	n = 0;
+	impares = 0;
+	pares = 0;
+	primos = 0;
+	soma =0;
 //Quantidade de numeros a serem inseridos
 	printf("Insira a quantidade de nuemros (maximo=20) a serem inseridos:\n");
 	scanf("%d", &n);
 	if ((n < 1) || (n > 20)) {
 		printf("Quantidade de numeros invalida!");
+		return 0;
 	}
 //Insercao dos numeros desejados
 	printf("Digitar os numeros (digete-o e pressione enter):");
-	j = 0;
 	for(i=0; i < n; i++) {
-		scanf("%d", &vet[j]);
-		j++;
+		scanf("%d", &vet[i]);
 	}
 //Analise dos nuemros inseridos
 	j = 0;
-	soma = 0;
 	for(i=0; i < n; i++) {
 //Contagem de quantos sao Pares e/ou Impares
 		if((vet[j] % 2) == 0)
@@ -26,11 +31,12 @@ int main() {
 		else
 			impares++;
 //Identificacao de quantos sao primos	
-		for (k = 2; k < vet[j]; k++) {// Arrumar esta parte que nao esta funcionando
-			if (vet[j] % k == 0) 
-				teste = 1;
+		for ( c = 2; c <= (vet[j]-1); c++) {
+			if(vet[i] % c == 0) {
+				break;
+			}
 		}
-		if (!teste)
+		if (c == vet[j])
 			primos++;
 //Soma total dos numeros
 		soma = soma + vet[j];
